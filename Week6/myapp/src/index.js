d5a45e945c63eaf154ee026b6852a75476479a38
1234512345-ip <ichchhapaneru@gmail.com>
2025-11-06 16:56:23 +0000

    commit

--- Week6/myapp/src/index.js
+++ Week6/myapp/src/index.js
@@ -8,7 +8,7 @@ import Calculator from './SimpleCalculator'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator/>
   </React.StrictMode>
 );
 
